99bac4ccbedd3878ef2c85f26efd5d7a26169698
Daniel Lobo <info@danlobo.co.uk>
2021-02-08 16:51:53 +0000

    - add fizzbuzz++ code

---
+++ fizzbuzz++/app.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+var log4js = require("log4js");
+var logger = log4js.getLogger();
+logger.level = "debug";
+
+log4js.configure({
+  appenders: {
+    output: { type: 'file', filename: 'fizzbuzz.log', layout: { type: 'messagePassThrough' }},
+    'out': { type: 'stdout', layout: { type: 'basic' } }
+  },
+  categories: { default: { appenders: ['output'], level: 'info' } }
+});
+
+
+function isPrime(num) {
+  if (num < 2) return false;
+
+  for (let k = 2; k < num; k++) {
+    if (num % k == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function fizzBuzz(n) {
+  // The script should echo the numbers 1 to 500, each number being on a new line.
+  console.time("FizzBuzz++ 1-500");
+  for(i=1; i <= n; i++) {
+    if(isPrime(i)){
+      console.log(i + " FizzBuzz++");
+      logger.info(i + " FizzBuzz++");
+    }
+    else {
+      if (i % 5 === 0 && i % 3 === 0) {
+        console.log(i + " FizzBuzz");
+        logger.info(i + " FizzBuzz");
+      } else {
+        if (i % 3 === 0) {
+          console.log(i + " Fizz");
+          logger.info(i + " Fizz");
+        }
+        else {
+          if (i % 5 === 0) {
+            console.log(i + " Buzz");
+            logger.info(i + " Buzz");
+          }
+          else {
+            console.log(i);
+            logger.info(i);
+          }
+        }
+
+      }
+    }
+
+
+  }
+  console.timeEnd("FizzBuzz++ 1-500");
+}
+
+fizzBuzz(500);
+
+
